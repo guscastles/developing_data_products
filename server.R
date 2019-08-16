9db0eb499ff4357db70d0c058d7db210632c9fb5
guscastles <guscastles@gmail.com>
2019-08-16 22:05:09 +1000

    Updated server, fixed predicted teams and goals

--- server.R
+++ server.R
@@ -25,7 +25,7 @@ shinyServer(function(input, output) {
         }
         
         predictedGoals <- function(data, team) {
-            data %>% filter(Winner == team) %>% select(Goals) %>% sapply(median)
+            data %>% filter(Winner == team) %>% select(Goals) %>% sapply(median) %>% as.integer
         }
         
         if (input$totalGoals == "Per Tournament") {
@@ -37,8 +37,8 @@ shinyServer(function(input, output) {
             winnersModel <- rpart(Winner ~ Year + Goals, data = data, method = "class")
             goals <- data$Goals %>% median
             prediction <- predict(winnersModel, data.frame(Year=c(2022, 2026), Goals=c(goals, goals)))
-            team1 <- predictedTeam(prediction[1,])
-            team2 <- predictedTeam(prediction[2,])
+            team1 <- predictedTeam(prediction[1,])[1]
+            team2 <- predictedTeam(prediction[2,])[2]
             goals1 <- predictedGoals(data, team1)
             goals2 <- predictedGoals(data, team2)
             rbind(data, data.frame(Year=c(2022, 2026), Winner=c(team1, team2), Goals=c(goals1, goals2))) %>%
@@ -47,14 +47,14 @@ shinyServer(function(input, output) {
     }
     
     perTournamentData <- function(data) {
-        dt <- data %>% select(Year, Goals.for, Penalty.goal) %>% mutate(Goals = Penalty.goal + Goals.for)
+        dt <- data %>% select(Year, Goals.for, Penalty.goal) %>% mutate(Goals = Goals.for)
         list(data=aggregate(Goals ~ Year, data=dt, FUN= sum), nudge_y=-20, colour="white")
     }
     
     perWinnerData <- function(data, winners) {
         plotData <- merge(data, winners, by="Year") %>% select(Team, Year, Winner, Goals.for, Penalty.goal) %>%
-            mutate(Goals = Goals.for + Penalty.goal) %>% filter(Team == Winner) %>% select(Year, Winner, Goals)
-        list(data=plotData, nudge_y=.5, colour="darkblue")
+            mutate(Goals = Goals.for) %>% filter(Team == Winner) %>% select(Year, Winner, Goals)
+        list(data=plotData, nudge_y=1.5, colour="darkblue")
     }
     
     fetchData <- function() {
@@ -67,23 +67,21 @@ shinyServer(function(input, output) {
         xMax <- input$editions[2]
         data <- fetchData()
         dataByDates <- data[(data$Year >= xMin) & (data$Year <= xMax),]
-        plotData <- if (input$totalGoals == "Per Tournament") perTournamentData(data)
-                    else perWinnerData(data, winnersData)
-        dateFilter <- (plotData[["data"]]$Year >= xMin) & (plotData[["data"]]$Year <= xMax)
+        plotData <- if (input$totalGoals == "Per Tournament") perTournamentData(data) else perWinnerData(data, winnersData)
         plotData[["predicted"]] <- plotData[["data"]] %>% predictNextEditions(xMin, xMax) %>%
                                        filter(Year >= xMin, Year <= xMax)
         xLab <- "Year"
         yLab <- "Goals Scored"
         mainTitle <- paste("Goals Scored", input$totalGoals)
         xLabels = seq(xMin, xMax, 4)
-        g <- ggplot(plotData[["predicted"]], aes(x=Year, y=Goals))
-        g <- g + geom_bar(stat = "identity", aes(fill = Year)) +
+        g <- ggplot(plotData[["predicted"]], aes(Year, Goals)) +
+                geom_bar(stat = "identity", aes(fill = Year)) +
                 geom_line(color = "red", lwd = 2) + xlab(xLab) + ylab(yLab) + ggtitle(mainTitle) +
                 scale_x_continuous(breaks=xLabels, labels=xLabels) + theme(legend.position = "none") +
                 geom_text(aes(label=Goals, fontface="bold"), colour = plotData[["colour"]],
                           nudge_y = plotData[["nudge_y"]])
         if (input$totalGoals == "Per Winner") {
-            g <- g + geom_text(aes(label=Winner, fontface="bold", angle=90), colour = "white", nudge_y = -1.5)
+            g <- g + geom_text(aes(label=Winner, fontface="bold", angle=90), colour = "white", nudge_y = -3)
         }
         g
     })
